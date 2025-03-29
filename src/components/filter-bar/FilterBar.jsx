4f8f38afd7a0e2b04d383231cda233c7706d8df6
Jose Luis Muziotti <joselmuziotti@gmail.com>
2025-03-29 11:56:42 +0100

    create filter component

---
+++ src/components/filter-bar/FilterBar.jsx
@@ -0,0 +1,16 @@
+import './index.css'
+
+const FilterBar = () => {
+  return (
+    <section>
+      <span>Extensions List</span>
+        <ul>
+          <button>All</button>
+          <button>Active</button>
+          <button>Inactive</button>
+        </ul>
+  </section>
+  )
+}
+
+export default FilterBar
