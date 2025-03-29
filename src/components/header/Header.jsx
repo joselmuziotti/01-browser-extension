4fe1babceb59d43628057c379c9a39d6e70dabfd
Jose Luis Muziotti <joselmuziotti@gmail.com>
2025-03-29 09:27:23 +0100

    create component & styles

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './index.css'
+
+const Header = () => {
+  return (
+      <header>
+        <article>
+          <img src='/images/logo.svg' alt="Logo" />
+
+          <button>
+            <img src='/images/icon-sun.svg' alt="Logo Sun" />
+          </button>
+
+        </article>
+      </header>
+  )
+}
+
+export default Header
