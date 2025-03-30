183e4a9f974f3489cbb4f671d41770d5319c8ad7
Jose Luis Muziotti <joselmuziotti@gmail.com>
2025-03-30 12:33:07 +0200

    add code & styles for board

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,27 @@ function App() {
     <>
   <Header />
   <FilterBar />
+    <main>
+      <section>
+
+        <article>
+          <img src="/images/logo-devlens.svg" alt="" />
+            <div>
+              <span>DevLens</span>
+              <p>Quickly inspect page layouts and visualize element boundaries.</p>
+            </div>
+        </article>
+
+        <div className='bottom-card' >
+          <button>Remove</button>
+            <label className="switch">
+              <input type="checkbox" />
+              <span className="slider"></span>
+            </label>
+        </div>
+
+      </section>
+    </main>
   </>
   )
 }
