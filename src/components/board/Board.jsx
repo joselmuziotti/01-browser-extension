2f1c14960270098dd0e32d803341289e4507391a
Jose Luis Muziotti <joselmuziotti@gmail.com>
2025-03-30 13:28:42 +0200

    create board component

---
+++ src/components/board/Board.jsx
@@ -0,0 +1,23 @@
+import Card from '../card/Card'
+import './index.css'
+
+const Board = () => {
+  return (
+    <main>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </main>
+  )
+}
+
+export default Board
