34fddde70c3bb2d364e0d0caac75ddb207324ef9
Jose Luis Muziotti <joselmuziotti@gmail.com>
2025-04-01 13:55:58 +0200

    add logic for themes modes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,32 @@
-import React from 'react'
-import './index.css'
+import { useState, useEffect } from "react";
+import "./index.css";
 
 const Header = () => {
+  const [theme, setTheme] = useState(
+    window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"
+  );
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "dark" ? "light" : "dark"));
+  };
+
   return (
-      <header>
-        <article>
-          <img src='/images/logo.svg' alt="Logo" />
+    <header>
+      <article>
+        <img src={`/images/logo-${theme}.svg`} alt="Logo" />
+        <button onClick={toggleTheme}>
+          <img
+            src={`/images/icon-${theme}.svg`}
+            alt={`Logo ${theme === "light" ? "Sun" : "Moon"}`}
+          />
+        </button>
+      </article>
+    </header>
+  );
+};
 
-          <button>
-            <img src='/images/icon-sun.svg' alt="Logo Sun" />
-          </button>
-
-        </article>
-      </header>
-  )
-}
-
-export default Header
+export default Header;
