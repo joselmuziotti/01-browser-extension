8385a7e3e9ff8332481736cbc3b7c27528c4bd1a
Jose Luis Muziotti <joselmuziotti@gmail.com>
2025-04-05 14:07:27 +0200

    ft: filter function & code optimization

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -1,11 +1,47 @@
+import { useState } from 'react'
 import Card from '../card/Card'
+import FilterBar from '../filter-bar/FilterBar'
+import data from '../../js/data.json'
 import './index.css'
 
 const Board = () => {
+  const [info, setInfo] = useState(data);
+  const [filter, setFilter] = useState('all');
+
+
+  const toggleActive = (name) => {
+    const updated = info.map((filter) =>
+      filter.name === name ? { ...filter, isActive: !filter.isActive } : filter
+    );
+    setInfo(updated);
+  }
+
+  const filteredExtensions = info.filter(ext => {
+    if (filter === 'all') return true
+    return ext.isActive === filter
+  })
+  
   return (
+    <>
+    <FilterBar setFilter={setFilter} />
+
     <main>
-      <Card />
+
+    {filteredExtensions.map((info) =>
+
+      <Card 
+      key={info.name}
+      name={info.name}
+      logo={info.logo}
+      description={info.description}
+      isActive={info.isActive}
+      toggleActive={toggleActive}
+      />)
+      
+      };
+
     </main>
+    </>
   )
 }
 
